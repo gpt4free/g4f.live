4f3b233de42560b5c2eb37f4ec05d453088e260a
hlohaus <983577+hlohaus@users.noreply.github.com>
2025-06-15 07:13:49 +0200

    Add backup support

--- dist/js/chat.v1.js
+++ dist/js/chat.v1.js
@@ -1517,8 +1517,9 @@ async function set_conversation_title(conversation_id, title) {
     if (new_id && !appStorage.getItem(`conversation:${new_id}`)) {
         appStorage.removeItem(`conversation:${conversation.id}`);
         title_ids_storage[conversation_id] = new_id;
+        conversation.backup = conversation.backup || conversation.id;
         conversation.id = new_id;
-        add_url_to_history(`#${conversation_id}`);
+        add_url_to_history(`#${new_id}`);
     }
     appStorage.setItem(
         `conversation:${conversation.id}`,

3d9263de7eda3912443ac7aeb77d18dd2a227ff1
H Lohaus <hlohaus@users.noreply.github.com>
2025-06-18 18:31:51 +0200

    Update framework.js

--- dist/js/framework.js
+++ dist/js/framework.js
@@ -12,8 +12,8 @@ if (window.location.protocol === "file:") {
 if (["https:", "http:"].includes(window.location.protocol)) {
     checkUrls.push(window.location.origin);
 }
-checkUrls.push("https://phone.g4f.dev");
 checkUrls.push("https://host.g4f.dev");
+checkUrls.push("https://phone.g4f.dev");
 checkUrls.push("https://home.g4f.dev");
 async function checkUrl(url, connectStatus) {
     let response;
